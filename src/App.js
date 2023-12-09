f6bfab4faf5685b66bd4178b54d2ed1e81337960
SJM <simjung418@gmail.com>
2023-12-09 17:20:20 +0900

    - 1번 완료

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Start React 200!</h1>
+      <p>HTML 적용하기</p>
     </div>
   );
 }
